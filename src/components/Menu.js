60a0518bce358e43a522384e7143efd7e78d4fdc
AzureOrange404 <AzureOrange404@protonmail.com>
2021-03-13 17:48:07 +0000

    Update Menu.js

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -7,6 +7,9 @@ const Menu = (props) => (
         <div className="inner">
             <ul className="links">
                 <li><Link onClick={props.onToggleMenu} to="/">Home</Link></li>
+                <li><Link onClick={props.onToggleMenu} to="/about">About Us</Link></li>
+                <li><Link onClick={props.onToggleMenu} to="/blog">Blog</Link></li>
+                <li><Link onClick={props.onToggleMenu} to="/rideouts">Rideouts</Link></li>
                 <li><Link onClick={props.onToggleMenu} to="/landing">Landing</Link></li>
                 <li><Link onClick={props.onToggleMenu} to="/generic">Generic</Link></li>
                 <li><Link onClick={props.onToggleMenu} to="/elements">Elements</Link></li>
