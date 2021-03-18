ad6d4bf66497638ad7dca3586e5fcdbf2bca663c
AzureOrange404 <AzureOrange404@protonmail.com>
2021-03-18 23:15:55 +0100

    Update Banner.js

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,18 +1,14 @@
 import React from 'react'
 
 const Banner = (props) => (
-    <div>
-	<link rel="stylesheet" href="dynamictitle.css">pumpernickel</link>
 
 	<section id="banner" className="major">
 		<div className="inner">
 
 		    <header className="wrapperino">
-			<h1>Let's ride <spanx>for fuck's sake </spanx></h1><h1 class="wordchanger"></h1>
+			<h1>Let's ride <spanx>for fuck's sake </spanx></h1>
 		    </header>
-			<div class="wrapperino">
-			<h1x>ride <spanx></spanx></h1x>
-			</div>
+			
 		    <div className="content">
 			<ul className="actions">
 			    <li><a href="#one" className="button next scrolly">Get Ridin'</a></li>
@@ -20,7 +16,6 @@ const Banner = (props) => (
 		    </div>
 		</div>
 	</section>
-    </div>
 )
 
 export default Banner
