63a00aac65f558d8673bf71df5030d924c4d50ff
AzureOrange404 <AzureOrange404@protonmail.com>
2021-03-18 15:08:38 +0100

    Update Banner.js

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -9,7 +9,7 @@ const Banner = (props) => (
             </header>
 	
             <div className="content">
-                <p>A group of young and motivated <b>Motorcyclists</b><br>
+                <p>A group of young and motivated <strong>Motorcyclists</strong>
 		(EN: <sub>[NOUN] [SINGULAR]</sub> Motorcyclist, IPA: [ˈmoʊtɚˌsaɪ.klɪst]:
                 A person willing to take a container of flammable liquid, place it on top of a hot moving engine and the put the whole lot between their legs.).</p>
                 <ul className="actions">
