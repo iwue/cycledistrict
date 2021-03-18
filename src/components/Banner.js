b51c2bbc0cca83b44364905a30efaa4ecc6d2ffa
AzureOrange404 <AzureOrange404@protonmail.com>
2021-03-18 13:12:40 +0100

    Description

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -28,7 +28,11 @@ const Banner = (props) => (
                 <h1 class="dyntext"></h1>
             </header>
             <div className="content">
-                <p>A group of young and motivated motorcyclists.</p>
+                <p>
+                A group of young and motivated <b>Motorcyclists</b> 
+		        (EN: <sub>NOUN SINGULAR</sub> Motorcyclist, <sub>IPA</sub>: [ˈmoʊtɚˌsaɪ.klɪst]: 
+                A person willing to take a container of flammable liquid, place it on top of a hot moving engine and the put the whole lot between their legs.).
+                </p>
                 <ul className="actions">
                     <li><a href="#one" className="button next scrolly">Get Started</a></li>
                 </ul>
