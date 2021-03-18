da2a688209e46ed63073a2452211f55de04e2d3c
AzureOrange404 <AzureOrange404@protonmail.com>
2021-03-18 16:56:18 +0100

    Update Banner.js

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -5,14 +5,11 @@ const Banner = (props) => (
         <div className="inner">
 	    
             <header className="wrapperino">
-                <h1>ride <spanx>for fucks sake</spanx></h1>
+                <h1>ride <spanx>for fuck's sake</spanx></h1>
             </header>
 	
             <div className="content">
-                /* <p>A group of young and motivated <b>Motorcyclists</b><br/>
-		(EN: [NOUN] [SINGULAR] Motorcyclist, IPA: [moh-ter-sahy-klist]: *//* ˈmoʊtɚˌsaɪ.klɪst *//*
-                A person willing to take a container of flammable liquid, place it on top of a hot moving engine and then put the whole lot between their legs.).</p>
-		*/
+                <p></p>
                 <ul className="actions">
                     <li><a href="#one" className="button next scrolly">Get Ridin'</a></li>
                 </ul>
