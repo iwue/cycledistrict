fd4f0f003bcd7edb45d896a68ea7113103b1f1ce
AzureOrange404 <AzureOrange404@protonmail.com>
2021-03-18 18:32:37 +0100

    Update Banner.js

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -5,7 +5,7 @@ const Banner = (props) => (
         <div className="inner">
 	    
             <header className="wrapperino">
-                <h1>ride <spanx>for fuck's sake</spanx></h1>
+                <h1>Let's ride <spanx>for fuck's sake</spanx></h1>
             </header>
 	
             <div className="content">
