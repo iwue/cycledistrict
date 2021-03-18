d3fb3b4c9bf3caefb036197895a15d5fa90bb434
AzureOrange404 <AzureOrange404@protonmail.com>
2021-03-18 15:34:02 +0100

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -31,45 +31,45 @@ class HomeIndex extends React.Component {
                     <section id="one" className="tiles">
                         <article style={{backgroundImage: `url(${pic01})`}}>
                             <header className="major">
-                                <h3>Aliquam</h3>
-                                <p>Ipsum dolor sit amet</p>
+                                <h3>Blog</h3>
+                                <p>What is going on in CycleDistrict East?</p>
                             </header>
-                            <Link to="/landing" className="link primary"></Link>
+                            <Link to="/blog" className="link primary"></Link>
                         </article>
                         <article style={{backgroundImage: `url(${pic02})`}}>
                             <header className="major">
-                                <h3>Tempus</h3>
-                                <p>feugiat amet tempus</p>
+                                <h3>Newest Blogpost</h3>
+                                <p>[Sneak Peak]</p>
                             </header>
-                            <Link to="/landing" className="link primary"></Link>
+                            <Link to="/blog" className="link primary"></Link>
                         </article>
                         <article style={{backgroundImage: `url(${pic03})`}}>
                             <header className="major">
-                                <h3>Magna</h3>
-                                <p>Lorem etiam nullam</p>
+                                <h3>About Us</h3>
+                                <p>What is CycleDistrict East?</p>
                             </header>
-                            <Link to="/landing" className="link primary"></Link>
+                            <Link to="/about" className="link primary"></Link>
                         </article>
                         <article style={{backgroundImage: `url(${pic04})`}}>
                             <header className="major">
-                                <h3>Ipsum</h3>
-                                <p>Nisl sed aliquam</p>
+                                <h3>Riders</h3>
+                                <p>Who is CycleDistrict East?</p>
                             </header>
-                            <Link to="/landing" className="link primary"></Link>
+                            <Link to="/about" className="link primary"></Link>
                         </article>
                         <article style={{backgroundImage: `url(${pic05})`}}>
                             <header className="major">
-                                <h3>Consequat</h3>
+                                <h3>Placeholder</h3>
                                 <p>Ipsum dolor sit amet</p>
                             </header>
                             <Link to="/landing" className="link primary"></Link>
                         </article>
                         <article style={{backgroundImage: `url(${pic06})`}}>
                             <header className="major">
-                                <h3>Etiam</h3>
-                                <p>Feugiat amet tempus</p>
+                                <h3>Rideouts</h3>
+                                <p>When will we be seen?</p>
                             </header>
-                            <Link to="/landing" className="link primary"></Link>
+                            <Link to="/rideouts" className="link primary"></Link>
                         </article>
                     </section>
                     <section id="two">
@@ -77,9 +77,11 @@ class HomeIndex extends React.Component {
                             <header className="major">
                                 <h2>Massa libero</h2>
                             </header>
-                            <p>Nullam et orci eu lorem consequat tincidunt vivamus et sagittis libero. Mauris aliquet magna magna sed nunc rhoncus pharetra. Pellentesque condimentum sem. In efficitur ligula tate urna. Maecenas laoreet massa vel lacinia pellentesque lorem ipsum dolor. Nullam et orci eu lorem consequat tincidunt. Vivamus et sagittis libero. Mauris aliquet magna magna sed nunc rhoncus amet pharetra et feugiat tempus.</p>
+                            <p>A group of young and motivated <b>Motorcyclists</b><br/>
+		                    (EN: [NOUN] [SINGULAR] Motorcyclist, IPA: [ˈmoʊtɚˌsaɪ.klɪst]: /* ˈmoʊtɚˌsaɪ.klɪst -- moh-ter-sahy-klist */
+                            A person willing to take a container of flammable liquid, place it on top of a hot moving engine and then put the whole lot between their legs.).</p>
                             <ul className="actions">
-                                <li><Link to="/landing" className="button next">Get Started</Link></li>
+                                <li><Link to="/generic" className="button next">Get Ridin'</Link></li>
                             </ul>
                         </div>
                     </section>
