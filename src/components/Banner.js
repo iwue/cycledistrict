c64ecc1af69301818b4bb327b7ef3c2e6406142f
AzureOrange404 <AzureOrange404@protonmail.com>
2021-03-18 22:56:06 +0100

    Update Banner.js

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,11 +1,35 @@
 import React from 'react'
 
 const Banner = (props) => (
+    <head>
+	<meta charset="utf-8">
+	<title>Changing Word Slider</title>
+
+	<style>
+	 .wordchanger:before{
+	  content: 'content.';
+	  animation-name: mywordchange;
+	  animation-duration: 10s;
+	  animation-iteration-count: infinite;
+	  animation-timing-function: ease;
+	 }
+
+	 @keyframes mywordchange{
+	  0%{content: 'CycleDistrict.';}
+	  10%{content: 'fast.';}
+	  20%{content: 'free.';}
+	  30%{content: 'trashy.';}
+	  40%{content: 'CycleDistrict.';}
+	  100%{content: 'CycleDistrict.';}
+	 }
+	</style>
+    </head>
+    <body>
     <section id="banner" className="major">
         <div className="inner">
 	    
             <header className="wrapperino">
-                <h1>Let's ride <spanx>for fuck's sake</spanx></h1>
+                <h1>Let's ride <spanx>for fuck's sake </spanx></h1><h1 class="wordchanger"></h1>
             </header>
 	
             <div className="content">
@@ -15,6 +39,7 @@ const Banner = (props) => (
             </div>
         </div>
     </section>
+    </body>
 )
 
 export default Banner
