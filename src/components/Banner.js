c003e6f31b3fef9868fb061a0e6e1519822bb303
AzureOrange404 <AzureOrange404@protonmail.com>
2021-03-18 14:58:42 +0100

    Update Banner.js

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -5,13 +5,13 @@ const Banner = (props) => (
         <div className="inner">
 	    
             <header className="wrapperino">
-                <h1>ride <spanx></spanx></h1>
+                <h1>ride <spanx>for fucks sake</spanx></h1>
             </header>
 	
             <div className="content">
                 <p>
-                A group of young and motivated <b>Motorcyclists</b> 
-		        (EN: <sub>NOUN SINGULAR</sub> Motorcyclist, <sub>IPA</sub>: [ˈmoʊtɚˌsaɪ.klɪst]: 
+                A group of young and motivated <b>Motorcyclists</b></br>
+		(EN: <sub>[NOUN] [SINGULAR]</sub> Motorcyclist, IPA: [ˈmoʊtɚˌsaɪ.klɪst]:</br>
                 A person willing to take a container of flammable liquid, place it on top of a hot moving engine and the put the whole lot between their legs.).
                 </p>
                 <ul className="actions">
