fd6139e900669702d815ceed06a2a7be56cb3fb8
AzureOrange404 <AzureOrange404@protonmail.com>
2021-03-18 14:11:29 +0100

    Dynamic title 2.1

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -3,10 +3,23 @@ import React from 'react'
 const Banner = (props) => (
     <section id="banner" className="major">
         <div className="inner">
-            <header className="major">
-                <h1>ride </h1>
-                <h1>fast.</h1>
+	    <head>
+		<meta charset="utf-8">
+		<title>Changing Word Slider</title>
+		<link rel="stylesheet" href="dynamictitle.css">
+	    </head>
+            <header class="wrapperino">
+                <h1x>ride <spanx></spanx></h1x>
             </header>
+		<div class="wrapperino">
+			<h1x>ride <spanx></spanx></h1x>
+			<p>A group of young and motivated <b>Motorcyclists</b> 
+			(EN: <sub>NOUN SINGULAR</sub> Motorcyclist, <sub>IPA</sub>: [ˈmoʊtɚˌsaɪ.klɪst]:
+			A person willing to take a container of flammable liquid,
+			place it on top of a hot moving engine
+			and then put the whole lot between their legs.)
+			</p>
+		</div>
             <div className="content">
                 <p>
                 A group of young and motivated <b>Motorcyclists</b> 
