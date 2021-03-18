9d70af82b6cd288d93e22066164a1823e6c0113f
AzureOrange404 <AzureOrange404@protonmail.com>
2021-03-18 23:05:12 +0100

    Update Banner.js

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -4,22 +4,24 @@ const Banner = (props) => (
     <head>
 	<link rel="stylesheet" href="dynamictitle.css">pumpernickel</link>
     </head>
-    <section id="banner" className="major">
-        <div className="inner">
-	    
-            <header className="wrapperino">
-                <h1>Let's ride <spanx>for fuck's sake </spanx></h1><h1 class="wordchanger"></h1>
-            </header>
-		<div class="wrapperino">
-		<h1x>ride <spanx></spanx></h1x>
+    <body>
+	<section id="banner" className="major">
+		<div className="inner">
+
+		    <header className="wrapperino">
+			<h1>Let's ride <spanx>for fuck's sake </spanx></h1><h1 class="wordchanger"></h1>
+		    </header>
+			<div class="wrapperino">
+			<h1x>ride <spanx></spanx></h1x>
+			</div>
+		    <div className="content">
+			<ul className="actions">
+			    <li><a href="#one" className="button next scrolly">Get Ridin'</a></li>
+			</ul>
+		    </div>
 		</div>
-            <div className="content">
-                <ul className="actions">
-                    <li><a href="#one" className="button next scrolly">Get Ridin'</a></li>
-                </ul>
-            </div>
-        </div>
-    </section>
+	</section>
+    </body>
 )
 
 export default Banner
