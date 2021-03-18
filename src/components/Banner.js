013807fa5d776b5fd8778f5abcef245b60e1adca
AzureOrange404 <AzureOrange404@protonmail.com>
2021-03-18 13:10:46 +0100

    Dynamic Title

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -4,11 +4,31 @@ const Banner = (props) => (
     <section id="banner" className="major">
         <div className="inner">
             <header className="major">
-                <h1>Hi, my name is Forty</h1>
+                <meta charset="utf-8">
+    
+                <style>
+                    .dyntext:before{
+                        content: 'content.';
+                        animation-name: mydyntext;
+                        animation-duration: 10s;
+                        animation-iteration-count: infinite;
+                        animation-timing-function: ease;
+                    }
+
+                    @keyframes mydyntext{
+                        0%{content: 'CycleDistrict.';}
+                        10%{content: 'fast.';}
+                        20%{content: 'free.';}
+                        30%{content: 'trashy.';}
+                        40%{content: 'CycleDistrict.';}
+                        100%{content: 'CycleDistrict.';}
+                    }
+                </style>
+                <h1>ride </h1>
+                <h1 class="dyntext"></h1>
             </header>
             <div className="content">
-                <p>A responsive site template designed by HTML5 UP<br />
-                and released under the Creative Commons.</p>
+                <p>A group of young and motivated motorcyclists.</p>
                 <ul className="actions">
                     <li><a href="#one" className="button next scrolly">Get Started</a></li>
                 </ul>
