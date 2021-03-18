1cc2ee535ec2240320835e497fc8ec64dfde7b14
AzureOrange404 <AzureOrange404@protonmail.com>
2021-03-18 16:28:05 +0100

    Update about.js

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,39 +1,48 @@
 import React from 'react'
+import { Link } from 'gatsby'
 import Helmet from 'react-helmet'
 import Layout from '../components/layout'
 
+import pic01 from '../assets/images/pic01.jpg'
 
-const About = (props) => (
-    <Layout>
-        <Helmet>
-            <title>About</title>
-            <meta name="description" content="About Page" />
-        </Helmet>
+class About extends React.Component {
+    render() {
 
-        <div id="main" className="alt">
-            <section id="one">
-                <div className="inner">
-                    <header className="major">
-                        <h1>About Us</h1>
-                    </header>
-                    <p>We are just a bunch of young Swiss motorcylists.</p>
-                    <p> </p>
-                    <p> </p>
-                    <h2>FOUNDERS</h2>
+        return (
+
+        /* const About = (props) => ( */
+            <Layout>
+                <Helmet>
+                    <title>About</title>
+                    <meta name="description" content="About Page" />
+                </Helmet>
+
+                <div id="main" className="alt">
+                    <section id="one">
+                        <div className="inner">
+                            <header className="major">
+                                <h1>About Us</h1>
+                            </header>
+                            <p>We are just a bunch of young Swiss motorcylists.</p>
+                            <p> </p>
+                            <p> </p>
+                            <h2>FOUNDERS</h2>
+                        </div>
+                    </section>
+                    <section id="one" className="tiles">
+                        <article style={{backgroundImage: `url(${pic01})`}}>
+                            <header className="major">
+                                <h3>AzureOrange</h3>
+                                <p>Dragstar Rider</p>
+                            </header>
+                            <Link to="/ridersazureorange" className="link primary"></Link>
+                        </article>
+                    </section>
                 </div>
-            </section>
-            <section id="one" className="tiles">
-                <article>
-                    <header className="major">
-                        <h3>AzureOrange</h3>
-                        <p>Dragstar Rider</p>
-                    </header>
-                    <Link to="/ridersazureorange" className="link primary"></Link>
-                </article>
-            </section>
-        </div>
 
-    </Layout>
-)
+            </Layout>
+        )
+    }
+}
 
 export default About
