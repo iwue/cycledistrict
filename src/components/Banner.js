8f71807f478a82902ae5530d47ca39d4b6d6f46e
AzureOrange404 <AzureOrange404@protonmail.com>
2021-03-18 13:40:33 +0100

    One Step Back

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -4,31 +4,8 @@ const Banner = (props) => (
     <section id="banner" className="major">
         <div className="inner">
             <header className="major">
-	
-	<html lang="en" dir="ltr">
-                <meta charset="utf-8">
-    
-                
-                    .dyntext:before{
-                        content: 'content.';
-                        animation-name: mydyntext;
-                        animation-duration: 10s;
-                        animation-iteration-count: infinite;
-                        animation-timing-function: ease;
-                    }
-
-                    @keyframes mydyntext{
-                        0%{content: 'CycleDistrict.';}
-                        10%{content: 'fast.';}
-                        20%{content: 'free.';}
-                        30%{content: 'trashy.';}
-                        40%{content: 'CycleDistrict.';}
-                        100%{content: 'CycleDistrict.';}
-                    }
-                
                 <h1>ride </h1>
-                <h1 class="dyntext"></h1>
-	</html>
+                <h1>fast.</h1>
             </header>
             <div className="content">
                 <p>
