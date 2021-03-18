c766833be96d36c76e1ce48eccd72a700b4bcece
AzureOrange404 <AzureOrange404@protonmail.com>
2021-03-18 23:45:27 +0100

    Update musterblogeintrag.js

--- src/pages/musterblogeintrag.js
+++ src/pages/musterblogeintrag.js
@@ -1,1 +1,107 @@
+import React from 'react'
+import Helmet from 'react-helmet'
+import Layout from '../components/layout'
 
+
+const Muster = (props) => (
+    <Layout>
+        <Helmet>
+            <title>Blog - Muster</title>
+            <meta name="description" content="Riders Page" />
+        </Helmet>
+
+        <div id="main" className="alt">
+            <section id="one">
+                <div className="inner">
+                    <header className="major">
+                        <h1>Bikergruß</h1>
+                        <h4>by AzureOrange, 18<sup>th</sup> March 2021</h4>
+                    </header>
+                    <p>
+  <h3>Von Händen und Keulen</h3>
+Die Ursprünge des Motorradgrußes reichen bis in die Steinzeit zurück.
+Motorradfahrer waren damals außerordentlich rar. Es gab kaum befestigte Straßen, und die Räder waren aus Stein.
+Nur die ganz harten Kerle vertrugen die Strapazen des Motorradfahrens.
+  <br/>
+Begegneten sich zwei dieser Kerle hielten sie an, stiegen ab und reichten einander die geöffneten Hände, um
+zu zeigen, dass sich kein Faustkeil darin verbarg.
+  <br/>
+So war der Motorradgruß erfunden.
+  </p>
+  <p>
+  <h3>Von Winterfahrern und Weicheiern</h3>
+Unter ähnlich harten Bedingungen sind heute nur noch die Winterfahrer unterwegs.
+Somit sind Motorradfahrer entweder Winterfahrer oder Weicheier.
+  <br/>
+Weicheier trifft man im April bei der Versicherung, wo sie ihre stillgelegten Maschinen wieder anmelden.
+Winterfahrer dagegen fahren durch. Ihre Zahl ist klein.
+  <br/>
+Treffen sich zwei Winterfahrer, ist die Freude groß. Von April an grüßen Winterfahrer nicht mehr.
+Denn Winterfahrer grüßen keine Weicheier.
+Weicheier erkennt man daran, das sie in den ersten Frühlingswochen wie wild und beidhändig alles grüßen, was sich auf zwei Rädern bewegt.
+Vor lauter Aufregung vergessen sie dann oft, das da vorne eine Kurve kommt.
+Sie haben immer frische Unterwäsche an - man könnte ja im Krankenhaus landen.
+Der verbotene Gruß
+Das Motorradgrüßen ist stark reglementiert und wird von Anfängern zu Recht als sehr kompliziert angesehen.
+Es ist umlagert von allerlei Ge- und Verboten. Das bekannteste Verbot lautet: Grüße nie ein Einspurfahrzeug, das weniger als 250 ccm Hubraum hat. So etwas ist kein Motorrad!
+Wer fahrlässig Motorroller, Klein - oder Leichtkrafträder grüßt, verliert sein Gesicht und jegliche Selbstachtung.
+Ein Spezialfall: Oldtimer
+Oldtimer werden grundsätzlich freudig und bewundernd gegrüßt, unabhängig vom Hubraum.
+Oldtimer werden meist von technisch versierten älteren Fahrern gefahren, sogenannten "alten Schraubern".
+Solchen wird Respekt gezollt. Trifft man alte Schrauber, wartet man, ob sie grüßen.
+Von Frühling bis Herbst grüßen viele nicht, weil sie Winterfahrer sind - Winterfahrer grüßen keine Weicheier.
+Der Autobahn - Gruß
+Ungeregelt und darum praktisch nicht existent ist die Motorradgrußkultur auf der Autobahn. Nicht einmal erfahrene
+Motorradfahrer können sagen, ob man entgegenkommende Motorräder über sechs Spuren und einen Grünstreifen
+hinweg grüßen muß. Fahrtechnisch problematisch wird das Grüßen beim Überholen.
+Die klassische Grußhand, die Linke, wird vom Überholten nicht gesehen.
+Grüßt man mit der Linken vorn am Körper vorbei nach rechts, tippen Autofahrer auf Heuschreckenschwärme oder Unterarmkrampf.
+Der Heizer - Gruß
+Der "Heizergruß" in extremer Schräglage (ein Knie berührt den Asphalt) gilt als sehr riskant.
+Er wird allgemein als Nachweis hoher Fahrkunst angesehen, aber man sollte vorher trotzdem frische Unterwäsche anziehen.
+Heiz-Grüßer fahren nur auf der äußersten Profilrille und vergleichen, wer die spitzesten Fußrasten vorweisen kann.
+Wer die Kunst des Heizergrußes nicht beherrscht und dennoch ausübt, riskiert seinen letzten, den sogenannten "goldenen Gruß".
+Gründe, warum Harley Fahrer nicht zurückwinken:
+1. die Garantie erlischt, wenn er nicht-amerikanischen Produkten zuwinkt
+2. mit dem dicken Leder und den Nieten kriegt er den Arm nicht hoch
+3. er grüßt prinzipiell keine Fahrer, die ihr Motorrad schon voll bezahlt haben
+4. er hat Angst, daß der Lenker abvibriert, wenn er ihn loslässt
+5. der Fahrtwind könnte die neuen Tätowierungen am Arm wegblasen
+6. er braucht `ne Ewigkeit um nachher wegen der Vibrationen den Griff wieder zu finden
+7. er kann nicht unterscheiden, ob der andere grüßt oder sich die Ohren zuhält.
+8. er hat wegen der Vibrationen einen Klettverschlußzwischen Lenkergriff und Handschuh
+9. er hat gerade im Wirtschaftteil der Zeitung entdeckt, dass Honda zu 60 Prozent an Harley beteiligt ist
+10. seit der Zwangsversteigerung seiner letzten Harley hasst er alle Leute, die Hände heben
+11. die Rolex könnte naß werden
+12. die linke Lenkerhälfte könnte geklaut werden
+13. die linke Lenkerhälfte wurde bereits geklaut und er hält sich am Tacho fest
+14. der ganze Lenker wurde bereits geklaut und er betet einen Rosenkranz
+15. er braucht beide Hände um mit den Fingern die nächste Rate abzuzählen
+16. er poliert gerade den Luftfilterdeckel
+Gründe, warum Goldwing-Fahrer nicht grüßen:
+1. laut Honda-Fahrer-Handbuch darf er den Lenker erst loslassen, wenn
+das Motorrad steht, der Zündschlüssel abgezogen,derHauptständer
+herausgeklappt und das Radio ausgeschaltet ist
+2. auf dem Armaturenbrett ist kein Knopf für "vollautomatisches Zurückwinken"
+3. er ist gerade eingeschlafen
+4. er kriegt den Arm wegen Altersschwäche nicht mehr hoch
+5. er hat übers Handy eine Konferenzschaltung mit seinem Broker und der CityBank.
+6. Mami hat verboten, Fremde zu grüßen
+7. er ist gerade mit Abzählen der Lämpchen am Christbaum beschäftigt
+8. er sortiert momentan seine CD-Sammlung
+9. die Hand vor der Antenne stört den Fernsehempfang
+10. er räumt gerade das Kaffeegeschirr in den Spüler
+11. wegen seiner Alzheimer weiß er anschließend nicht mehr, wohin mit der Hand
+12. er findet nachher zwischen all den Knöpfen, Schaltern und Hebeln den Lenker nicht mehr
+13. er wechselt gerade die Batterie vom Herzschrittmacher
+14. er sucht gerade im Bordcomputer, was "erhobene Hand eines fremden Motorradfahrers" bedeutet
+15. er grüßt immer, aber wegen dem Geraffel um den Lenker sieht keiner die Hand.
+                    </p>
+                </div>
+            </section>
+        </div>
+
+    </Layout>
+)
+
+export default Muster
