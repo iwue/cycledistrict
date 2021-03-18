1de210322e1bbaa8092f32168756d69470893bb8
AzureOrange404 <AzureOrange404@protonmail.com>
2021-03-18 19:00:28 +0100

    Update about.js

--- src/pages/about.js
+++ src/pages/about.js
@@ -29,13 +29,32 @@ class About extends React.Component {
                         </div>
                     </section>
                     <section id="two" className="Titless">
+                    <div className="inner">
+                            <header className="major">
+                                <h1>Our Riders</h1>
+                            </header>
                         <h2>FOUNDERS</h2>
+                    </div>
                     </section>
                     <section id="three" className="tiles">
                         <article style={{backgroundImage: `url(${pic01})`}}>
                             <header className="major">
                                 <h3>AzureOrange</h3>
                                 <p>Dragstar Rider</p>
+                            </header>
+                            <Link to="/ridersazureorange" className="link primary"></Link>
+                        </article>
+                    </section>
+                    <section id="four" className="Titless">
+                    <div className="inner">
+                        <h2>MEMBERS</h2>
+                    </div>
+                    </section>
+                     <section id="five" className="tiles">
+                        <article style={{backgroundImage: `url(${pic01})`}}>
+                            <header className="major">
+                                <h3>Member</h3>
+                                <p>Mt-07 Rider</p>
                             </header>
                             <Link to="/ridersazureorange" className="link primary"></Link>
                         </article>
