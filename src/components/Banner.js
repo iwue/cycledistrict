626eeac5a4bc9cbfba153a8a2cb62da40340a9b4
AzureOrange404 <AzureOrange404@protonmail.com>
2021-03-18 15:18:06 +0100

    Update Banner.js

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -9,9 +9,9 @@ const Banner = (props) => (
             </header>
 	
             <div className="content">
-                <p>A group of young and motivated <strong>Motorcyclists</strong><br/>
-		(EN: <sub>[NOUN] [SINGULAR]</sub> Motorcyclist, IPA: [ˈmoʊtɚˌsaɪ.klɪst]:
-                A person willing to take a container of flammable liquid, place it on top of a hot moving engine and the put the whole lot between their legs.).</p>
+                <p>A group of young and motivated <b>Motorcyclists</b><br/>
+		(EN: [NOUN] [SINGULAR] Motorcyclist, IPA: [moh-ter-sahy-klist]: /* ˈmoʊtɚˌsaɪ.klɪst */
+                A person willing to take a container of flammable liquid, place it on top of a hot moving engine and then put the whole lot between their legs.).</p>
                 <ul className="actions">
                     <li><a href="#one" className="button next scrolly">Get Ridin'</a></li>
                 </ul>
