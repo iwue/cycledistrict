994923e6e7648ef92ac693ed4ec474d33eb1aa6f
AzureOrange404 <AzureOrange404@protonmail.com>
2021-03-20 19:11:41 +0100

    Update gatsby-ssr.js

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -5,15 +5,3 @@
  */
 
 // You can delete this file if you're not using it
-
-const React = require("react")
-
-export const onRenderBody = ({ setHeadComponents }, pluginOptions) => {
-  setHeadComponents([
-    <script
-      src="https://code.jquery.com/jquery-3.6.0.min.js"
-      integrity="sha256-H+K7U5CnXl1h5ywQfKtSj8PCmoN9aaq30gDh27Xc0jk="
-      crossOrigin="anonymous">
-    </script>,
-  ])
-}
