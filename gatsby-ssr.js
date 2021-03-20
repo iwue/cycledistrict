d48899accc957e53bb31acfb085d667c02a15fa5
AzureOrange404 <AzureOrange404@protonmail.com>
2021-03-20 18:54:30 +0100

    Update gatsby-ssr.js

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -5,3 +5,11 @@
  */
 
 // You can delete this file if you're not using it
+
+<head>
+  <script
+    src="https://code.jquery.com/jquery-3.3.1.min.js"
+    integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8="
+    crossOrigin="anonymous"
+  />
+</head>
