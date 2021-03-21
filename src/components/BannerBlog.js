d4863c4c65ca4c27f7fce125a78734490dee5071
AzureOrange404 <AzureOrange404@protonmail.com>
2021-03-21 19:50:28 +0100

    Create BannerBlog.js

---
+++ src/components/BannerBlog.js
@@ -0,0 +1,17 @@
+
+import React from 'react'
+
+const BannerBlog = (props) => (
+    <section id="banner" className="style2">
+        <div className="inner">
+            <header className="major">
+                <h1>Blog</h1>
+            </header>
+            <div className="content">
+                <p>Tüdelü - Hier gibts nicht viel zu sehen.</p>
+            </div>
+        </div>
+    </section>
+)
+
+export default BannerBlog
