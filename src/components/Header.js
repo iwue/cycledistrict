1ba38ca71a03e765e1f2743c452589890cec42b9
AzureOrange404 <AzureOrange404@protonmail.com>
2021-03-21 20:04:26 +0100

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,7 +5,7 @@ import Logo from '../assets/images/logo.svg'
 
 const Header = (props) => (
     <header id="header" className="alt">
-        <Link to="/" className="logo" ><Logo style={{ height: 90, width: 150 }}></Logo></Link>
+    {/*<Link to="/" className="logo" ><Logo style={{ height: 90, width: 150 }}></Logo></Link>*/}
         <nav>
             <a className="menu-link" onClick={props.onToggleMenu} href="javascript:;">Menu</a>
         </nav>
