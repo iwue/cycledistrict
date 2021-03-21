9c9d39e6f68853e9ef0bf0c1ed7930ba6232f779
AzureOrange404 <AzureOrange404@protonmail.com>
2021-03-21 21:46:21 +0100

    Create contact.js

---
+++ src/pages/contact.js
@@ -0,0 +1,30 @@
+import React from 'react'
+import Helmet from 'react-helmet'
+import Layout from '../components/layout'
+import BannerRideouts from '../components/BannerRideouts'
+
+const Contact = (props) => (
+    <Layout>
+        <Helmet>
+            <title>Rideouts</title>
+            <meta name="description" content="Rideouts Page" />
+        </Helmet>
+    
+        <BannerRideouts />
+
+        <div id="main" className="alt">
+            <section id="one">
+                <div className="inner">
+                    <h2>Group Rideouts - Annual</h2>
+                    <p>At the following dates an annual group rideout is planned.</p>
+                    <p>1<sup>st</sup> Sunday of May</p>
+                    <p>1<sup>st</sup> Sunday of August</p>
+                    <p>1<sup>st</sup> Sunday of November</p>
+                </div>
+            </section>
+        </div>
+
+    </Layout>
+)
+
+export default Contact
