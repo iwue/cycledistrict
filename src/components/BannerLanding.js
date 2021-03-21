a912957253ac9a3536b39c9848aa3aa883e56abf
AzureOrange404 <AzureOrange404@protonmail.com>
2021-03-21 19:51:45 +0100

    Update BannerLanding.js

--- src/components/BannerLanding.js
+++ src/components/BannerLanding.js
@@ -15,3 +15,18 @@ const BannerLanding = (props) => (
 )
 
 export default BannerLanding
+
+const BannerBlog = (props) => (
+    <section id="banner" className="style2">
+        <div className="inner">
+            <header className="major">
+                <h1>Blog</h1>
+            </header>
+            <div className="content">
+                <p>Tüdelü - Hier gibts nicht viel zu sehen.</p>
+            </div>
+        </div>
+    </section>
+)
+
+export default BannerBlog
