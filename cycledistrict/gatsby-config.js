a1749f070d535ea38a27eb742a81dd8337cdbf5e
AzureOrange404 <AzureOrange404@protonmail.com>
2021-05-23 13:13:06 +0200

    Initial commit

---
+++ cycledistrict/gatsby-config.js
@@ -0,0 +1,47 @@
+module.exports = {
+  siteMetadata: {
+    title: "Gatsby Starter - Forty V2",
+    author: "Hunter Chang",
+    description: "A Gatsby.js V2 Starter based on Forty by HTML5 UP"
+  },
+  plugins: [
+    'gatsby-plugin-react-helmet',
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: 'gatsby-starter-default',
+        short_name: 'starter',
+        start_url: '/',
+        background_color: '#663399',
+        theme_color: '#663399',
+        display: 'minimal-ui',
+        icon: 'src/assets/images/website-icon.png', // This path is relative to the root of the site.
+      },
+    },
+    {
+      resolve: `gatsby-plugin-sass`,
+      options: {
+        implementation: require("sass"),
+      },
+    },
+    {
+      resolve: "gatsby-plugin-react-svg",
+      options: {
+        rule: {
+          include: /images\/.*\.svg/, // See below to configure properly
+          omitKeys: ['xmlnsDc', 'xmlnsCc', 'xmlnsRdf', 'xmlnsSvg', 'xmlnsSodipodi', 'xmlnsInkscape']
+        }
+      }
+    },  
+    {
+      resolve: `gatsby-plugin-intl`,
+      options: {
+        path: `${__dirname}/src/intl`,
+        languages: [`de`, `en`, `ch`],
+        defaultLanguage: `de`,
+        redirect: false,
+      },
+    },
+    'gatsby-plugin-offline',
+  ],
+}
---
+++ cycledistrict/src/components/Menu.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import { Link } from 'gatsby'
+import Language  from './Language'
+
+const Menu = (props) => (
+    <nav id="menu">
+        <div className="inner">
+            <ul className="links">
+                <li><Link onClick={props.onToggleMenu} to="/">Home</Link></li>
+                <li><Link onClick={props.onToggleMenu} to="/about">About Us</Link></li>
+                <li><Link onClick={props.onToggleMenu} to="/blog">Blog</Link></li>
+                <li><Link onClick={props.onToggleMenu} to="/rideouts">Rideouts</Link></li>
+            </ul>
+            <ul className="actions vertical">
+                <li><a href="/contact" className="button special fit">Contact</a></li>
+                <li><a href="#" className="button fit">Log In</a></li>
+            </ul>
+            <Language />
+        </div>
+        <a className="close" onClick={props.onToggleMenu} href="javascript:;">Close</a>
+    </nav>
+)
+
+Menu.propTypes = {
+    onToggleMenu: PropTypes.func
+}
+
+export default Menu
